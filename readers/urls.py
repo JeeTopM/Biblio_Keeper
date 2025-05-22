609de307d932278fe1da7817569ebb7f904c1df5
Лев Романович Панас <jieba2005@list.ru>
2025-05-22 13:14:00 +0300

    Добавлена кнопка "Подробнее" для книговорота

--- readers/urls.py
+++ readers/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.readers_home, name="readers_home"),
     path("create_readers", views.create_readers, name="create_readers"),
+    path('<int:pk>', views.ReadersDetailView.as_view(), name= 'readers-detail')
 ]
--- readers/views.py
+++ readers/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 
 # Create your views here.
 def readers_home(request):
     readers = Articles.objects.all()  # добавить срез [:5]
     return render(request, "readers/readers_home.html", {"readers": readers})
+
+class ReadersDetailView(DetailView):
+    model = Articles
+    template_name = 'readers/details_create_readers.html'
+    context_object_name = 'article'
 
 
 def create_readers(request):
